74318703da40d0eb0ee72838fa2d26b3f1e8f30c
Jay Anthon Rico <jayanthonrico23@gmail.com>
2021-12-16 10:48:38 +0800

    Styled the navbar

--- practice.js
+++ practice.js
@@ -1,26 +1,21 @@
 const redbutton = document.getElementById("navbarhamburger");
-const navbar = document.querySelector(".navbar");
 const navbarheader= document.querySelector(".header");
 
 navbarheader.classList.add("hide");
-navbar.classList.add("hide");
 redbutton.addEventListener ("click", () => {
  
   
   
-  if(navbar.classList.contains("show")==true){
+  if(navbarheader.classList.contains("show")==true){
 
     navbarheader.classList.remove("show");
-    navbar.classList.remove("show");
-    navbar.classList.add("hide");
     navbarheader.classList.add("hide");
   }
   else{
       
     navbarheader.classList.remove("hide");
     navbarheader.classList.add("show");
-    navbar.classList.add("show");
-    navbar.classList.remove("hide");
+
   }
 })  
 
